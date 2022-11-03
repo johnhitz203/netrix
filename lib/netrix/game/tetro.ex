c6fc1a02fe8ce4abad06482832f2c858e8d2fac3
paulo-valim <plvalim@gmail.com>
2022-11-04 00:40:10 +0100

    class of 11/04

--- lib/netrix/game/tetro.ex
+++ lib/netrix/game/tetro.ex
@@ -1,12 +1,32 @@
-defmodule Netrix.Game.Tetro do 
-  defstruct [shape: :z, rotation: 0, position: {5,1}]
+defmodule Netrix.Game.Tetro do
+  defstruct shape: :z, rotation: 0, position: {5, 1}
 
   def new do
-    __struct__(shape: random_shape()) 
+    __struct__(shape: random_shape())
   end
 
-  defp random_shape do 
-    ~w[l i j s z o t]a 
+  defp random_shape do
+    ~w[l i j s z o t]a
     |> Enum.random()
   end
-end 
+
+  def right(%{position: {x, y}} = tetro) do
+    %{tetro | position: {x + 1, y}}
+  end
+
+  def left(%{position: {x, y}} = tetro) do
+    %{tetro | position: {x - 1, y}}
+  end
+
+  def down(%{position: {x, y}} = tetro) do
+    %{tetro | position: {x, y + 1}}
+  end
+
+  def rotate() do
+    "i'm a homework"
+  end
+
+  def to_points(%{shape: :i}) do
+    [{2, 1}, {2, 2}, {2, 3}, {2, 4}]
+  end
+end
