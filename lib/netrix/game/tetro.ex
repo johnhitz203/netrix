cdfa2f57d7b818ac85f35e879e7ea957f7fe6e4f
johnhitz203 <johnhitz203@gmail.com>
2022-11-10 12:01:00 -0600

    Added PLAN.md

--- lib/netrix/game/tetro.ex
+++ lib/netrix/game/tetro.ex
@@ -1,12 +1,12 @@
-defmodule Netrix.Game.Tetro do 
+defmodule Netrix.Game.Tetro do
   defstruct [shape: :z, rotation: 0, position: {5,1}]
 
   def new do
-    __struct__(shape: random_shape()) 
+    __struct__(shape: random_shape())
   end
 
-  defp random_shape do 
-    ~w[l i j s z o t]a 
+  defp random_shape do
+    ~w[l i j s z o t]a
     |> Enum.random()
   end
-end 
+end
